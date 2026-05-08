b5c673b6c60735b5201df5958da79a6a1ad996fc
Siriusgreed <lubdhok18@gmail.com>
2026-05-08 20:38:30 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-script.js
+function showPassword() {
+    document.getElementById("password").innerHTML = "ilove69";
+}
+function showFullEmail() {
+    document.querySelector(".email").innerHTML = "lubdhok18@gmail.com";
+}
